a086e9b7e5d06d652bf680ccf878f0196688c6c9
SAGorev <SAGorev@greenatom.ru>
2026-06-10 10:00:50 +0500

    Ref. #create_class "Создание классов"

--- src/Cat.java
+++ src/Cat.java
@@ -1,3 +1,5 @@
 public class Cat extends Animal{
-
+    public Cat (String name,int age,double weight, String color){
+        super(name, age, weight, color);
+    }
 }
--- src/Dog.java
+++ src/Dog.java
@@ -1,2 +1,5 @@
 public class Dog extends Animal{
+    public Dog (String name,int age,double weight, String color) {
+        super(name, age, weight, color);
+    }
 }
