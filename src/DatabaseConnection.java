627b56b63b070d64778cd7306696dec247dcece9
SAGorev <SAGorev@greenatom.ru>
2026-08-05 12:51:44 +0500

    Ref.#CRUD с PostgreSQL гитигнор

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -3,10 +3,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    // ВАЖНО: Укажите здесь свой пароль от MySQL вместо "ваш_пароль"
-    private static final String URL = "jdbc:mysql://localhost:3306/animal_db?serverTimezone=UTC";
-    private static final String USER = "root";
-    private static final String PASSWORD = "postgres";
+    // Строка подключения именно для PostgreSQL!
+    private static final String URL = "jdbc:postgresql://localhost:5432/animal_db";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres"; // Ваш пароль от PostgreSQL
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
